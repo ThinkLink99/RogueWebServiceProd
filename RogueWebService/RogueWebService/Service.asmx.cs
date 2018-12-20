788b3ff7f943797bcc2dcb22fd67833da500d710
Trey Hall <linkin562@gmail.com>
2018-12-20 16:17:08 -0500

    added email function for error messages

--- RogueWebService/RogueWebService/Service.asmx.cs
+++ RogueWebService/RogueWebService/Service.asmx.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
 using System.Web.Services;
 
 #if DEBUG
@@ -8,6 +10,8 @@ using RogueWebService.RogueDBTableAdapters;
 
 namespace RogueWebService
 {
+    
+
     /// <summary>
     /// Summary description for Service
     /// </summary>
@@ -18,17 +22,64 @@ namespace RogueWebService
     // [System.Web.Script.Services.ScriptService]
     public class Service : System.Web.Services.WebService
     {
+        #region hidden
+        const string gmailPassword = "u1R6zNsg";
+        #endregion
+
         [WebMethod]
         public string HelloWorld()
         {
             return "Hello World";
         }
 
+        public void Email (string subject, string message, string email)
+        {
+            MailMessage mail = new MailMessage("RogueWS@gmail.com", email);
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.gmail.com";
+            client.Credentials = new System.Net.NetworkCredential("linkin562@gmail.com", gmailPassword);
+
+            mail.Subject = subject;
+            mail.Body = message;
+
+            client.Send(mail);
+        }
+
+        /// <summary>
+        /// Returns an array containing the USER_ID and DISPLAYNAME given by the username and password
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Author: Trey Hall
+        /// Date: 12/20/18
+        /// </remarks>
         [WebMethod]
         public object[] GetUserInfo (string username, string password)
         {
             USERSTableAdapter users = new USERSTableAdapter();
-            return new object[] { users.GetDataByLogin(username, password)[0]["USER_ID"], users.GetDataByLogin(username, password)[0]["USER_ID"] };
+
+            string id = "", displayname = "";
+
+            try
+            {
+                id = users.GetDataByLogin(username, password)[0]["USER_ID"].ToString();
+                displayname = users.GetDataByLogin(username, password)[0]["USER_ID"].ToString();
+            }
+            catch(Exception ex)
+            {
+                id = "";
+                displayname = "";
+
+                Email("RogueWS Error - GetUserInfo", "GetUserInfo broke with this exception: " + ex.Message, "linkin562@gmail.com");
+            }
+
+            object[] obj = new object[] { id, displayname };
+            return obj;
         }
 
         [WebMethod]
